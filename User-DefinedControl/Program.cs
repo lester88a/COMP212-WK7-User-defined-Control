708a5c678fc265f9a309a667259810319eddb58b
lester88a <lester88a@gmail.com>
2015-10-21 08:49:12 -0400

    User-defined control

---
+++ User-DefinedControl/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_DefinedControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * User-defined control
+             * 
+             * controls: 
+             *      built-in controls> [eg, TextBox, tables etc.]
+             *      
+             *      user-defined controls (they most important controls)>
+             *          user control [conposite control]
+             *          inherited control
+             *          owner-drawn control
+             */
+        }
+    }
+}
